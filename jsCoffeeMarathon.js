6ea6a65e52210ecdd29f26f6dc818b1ae7dd4299
AGD3 <49073762+AGD3@users.noreply.github.com>
2020-03-03 15:48:03 -0800

    Add files via upload

---
+++ jsCoffeeMarathon.js
@@ -0,0 +1,42 @@
+console.log("Coffe Please!");
+
+let coffeeType = "Medium Roast";
+
+console.log(coffeeType);
+
+var latte = 2;
+var mocha = 3;
+console.log(latte + mocha);
+console.log(latte>mocha);
+console.log(latte<mocha);
+
+let sugarFree;
+
+alert(sugarFree);
+
+let customer1 = "Kamala Khan", drinkChoice = "Chai", quantity  = 4, foodAllergy = false;
+
+console.log(`${customer1} ${drinkChoice} ${quantity} + ${foodAllergy}`);
+
+let chai = 2.50
+// console.log (chai*quantity);
+
+// chai = 2;
+// var chai = 2; 
+
+console.log (chai);
+
+// let total = drinkChoice + " " + quantity + " " + "$" + quantity * chai;
+
+
+console.log("+++++++++++++++++++++++++++++++++++++++");
+let total = "+ Your final Order is: " + quantity + " " + drinkChoice + " " + "for a total of $" + quantity * chai + ".";
+
+console.log (total);
+console.log("+++++++++++++++++++++++++++++++++++++++");
+
+console.log("Nice choice of " + drinkChoice + ", but we recommend a Sprite!");
+
+
+
+
